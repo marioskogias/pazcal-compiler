df8efb579e029c5988efce5504d26590c7d176f7
marioskogias <marioskogias@gmail.com>
2014-05-22 16:23:24 +0300

    Create dummy functions for optimize and final code in main

--- Main.ml
+++ Main.ml
@@ -1,10 +1,15 @@
+let optimize quad_l = quad_l
+
+let print_final_code file_d code = Printf.fprintf file_d "Final code is coming soon...\n"
+
 let main =
   let lexbuf = Lexing.from_channel stdin in
   try
-    Parser.pmodule Lexer.lexer lexbuf;
+    let quad_list = Parser.pmodule Lexer.lexer lexbuf; in
+    let optimized_quads = optimize quad_list in
+    print_final_code stdout optimized_quads;
     exit 0
   with Parsing.Parse_error ->
     Printf.eprintf "syntax error on line %d \n" lexbuf.Lexing.lex_curr_p.Lexing.pos_lnum ;
-    exit 1
-
+    exit 1  
 
