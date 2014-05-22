a7dfc9d89809bd86139b918702641606617e28d3
marioskogias <marioskogias@gmail.com>
2014-05-22 18:00:20 +0300

    Add the basic sceleton

--- FinalCode.ml
+++ FinalCode.ml
@@ -1,3 +1,35 @@
 open FinalSupport
 
-let print_final_code file_d code = Printf.fprintf file_d "Final code is coming soon...\n"
+(* Start code *)
+let start_code program_label= 
+  let start = Printf.sprintf "\
+  xseg\tsegment\tpublic 'code'\n\
+  \tassume\tcs : xseg, ds : xseg, ss : xseg\n\
+  \torg\t100h\n\
+  main\tproc\tnear\n\
+  \tcall\tnear ptr %s\n\
+  \tmov\tax, 4C00h\n\
+  \tint\t21h\n\
+  main endp\n"
+    program_label
+  in (Start start)
+
+(* End code *)
+let end_code = End "xseg ends\n\tend  main\n"
+
+let final_code_of_quad = function 
+    |_ -> [start_code "test"]
+    
+let rec create_assembly = function
+    | ([], assembly_list) -> assembly_list
+    | (a::quad_list, assembly_list) -> create_assembly (quad_list, assembly_list@(final_code_of_quad a))
+
+let rec print_final_code file_d code =
+    let rec print_help d = function
+        | (h::tail) -> Printf.fprintf d "%s" h; print_help d tail 
+        | [] -> () 
+    in 
+    let assembly_list = create_assembly(code, []) in
+    let assembly_string = (List.map string_of_final_t assembly_list) in
+    print_help file_d assembly_string ; 
+    Printf.fprintf file_d "Final code is coming soon...\n"
