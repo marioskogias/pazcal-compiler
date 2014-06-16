4f82c04602dcf3f4809c984cb3b89978f6ba563c
Marios Kogias <marioskogias@gmail.com>
2014-06-16 19:44:29 +0100

    Create string declaration assembly

--- CharString.ml
+++ CharString.ml
@@ -6,5 +6,45 @@ let special_char_asci c =
     |'0' -> 0
     |'\\' -> 92
     |'\'' -> 39
-    |'"'  -> 3
+    |'"'  -> 34
   in string_of_int code
+
+let explode s =
+  let rec expl i l =
+    if i < 0 then l else
+    expl (i - 1) (s.[i] :: l) in
+  expl (String.length s - 1) [];;
+
+let implode l =
+  let result = String.create (List.length l) in
+  let rec imp i = function
+  | [] -> result
+  | c :: l -> result.[i] <- c; imp (i + 1) l in
+  imp 0 l;;
+
+
+let escape_chars s =
+  let char_arr = explode s in
+  let rec help_fn = function
+    |([], [], res) -> List.rev res
+    |([], current, res) -> let new_s = implode (List.rev current) in help_fn([], [], (new_s::res))
+    |((h::t), current, res) ->
+        let no_special = 
+          if (String.length (Char.escaped h)) == 1 then true
+          else false in
+        if no_special then help_fn(t,(h::current),res)
+        else 
+          let escaped = string_of_int (Char.code h) in
+          let old = implode (List.rev current) in
+            help_fn(t, [], (escaped::old::res))
+  in help_fn(char_arr, [], []) 
+
+let declare_string label str = 
+  let escaped = escape_chars str in
+  let add_db s = Printf.sprintf "db '%s'\n" s in
+  let db_list = List.map add_db escaped in
+  let rec final = function
+    |([], s) -> s
+    |((h::t), "") -> final(t, label^" "^h)
+    |((h::t), s) -> final(t, s^"\t"^h)
+  in final (db_list, "")
