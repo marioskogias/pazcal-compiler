3b0339690a79c9fb2d41ebd3d261ea5c8f47eddc
Marios Kogias <marioskogias@gmail.com>
2014-06-17 11:44:50 +0100

    Add first version of read string

--- FinalCode.ml
+++ FinalCode.ml
@@ -150,8 +150,8 @@ let label = function
 let lib_funcs = ["putchar";"puts";
                  "WRITE_INT";"WRITE_CHAR";
                  "WRITE_BOOL";"WRITE_STRING";
-                 "READ_INT";"READ_BOOL";"getchar"(*;
-                 "READ_STRING";*
+                 "READ_INT";"READ_BOOL";"getchar";
+                 "READ_STRING"(*;
                  "strlen";"strcmp";"strcpy";"strcat"*)]
 
 let rec register_lib_functions = function
@@ -168,6 +168,7 @@ let rec register_lib_functions = function
      |"WRITE_CHAR" -> ignore(help_reg "WRITE_CHAR" "_print_char"); register_lib_functions t
      |"WRITE_BOOL" -> ignore(help_reg "WRITE_BOOL" "_print_bool"); register_lib_functions t
      |"WRITE_STRING" -> ignore(help_reg "WRITE_STRING" "_write_string"); register_lib_functions t
+     |"READ_STRING" -> ignore(help_reg "READ_STRING" "_readString"); register_lib_functions t
      |_ -> ignore(help_reg h h); register_lib_functions t
 
 let declare_lib_functions () = 
