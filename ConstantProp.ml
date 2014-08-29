052fcaf6aa7d55ba45191dc2dcc8b5457fb29ea7
marioskogias <marioskogias@gmail.com>
2014-08-29 20:37:23 +0300

    Add simple version of constantProp

---
+++ ConstantProp.ml
@@ -0,0 +1,65 @@
+open QuadTypes
+open Symbol
+open Error
+
+let constant_optimize q_list =
+    let get_constant_val = function
+    |Quad_entry e -> (
+            match e.entry_info with
+            |ENTRY_variable vinfo -> if vinfo.is_const then ( 
+                try Some(int_of_string vinfo.value)
+                with Failure "int_of_string" -> None)
+            else None
+            |_ -> None
+            )
+    |Quad_int value -> Some (int_of_string value)
+    |_ -> None
+
+    (*x+0=x, x*1=x*)
+    in let handle_trans quad =  
+    match quad with 
+    | Quad_calc (op, q1, q2, q) -> (
+            match op with
+            |"+" -> (
+                match (q1, q2) with
+                |(Quad_int v, _) -> if v=="0" then Quad_set(q2, q) else quad 
+                |(_, Quad_int v) -> if v=="0" then Quad_set(q1, q) else quad
+                |(_,_) -> quad
+               ) 
+            |"*" -> (
+                match (q1, q2) with
+                |(Quad_int v, _) -> 
+                    if v=="1" then Quad_set(q2, q)
+                        else if v=="0" then Quad_set(Quad_int "0", q) 
+                            else quad 
+                |(_, Quad_int v) ->
+                    if v=="1" then Quad_set(q1, q)
+                        else if v=="0" then Quad_set(Quad_int "0", q) 
+                            else quad 
+                |(_,_) -> quad
+               ) 
+            |_ ->quad
+            )
+    |_ -> internal "Not a quad_calc"; raise Terminate
+    
+    in let change_single_quad quad =
+    match quad with 
+    | Quad_calc (op, q1, q2, q) -> (
+            let calc = 
+            match op with
+            |"+" -> ( + )
+            |"-" -> ( - )
+            |"*" -> ( * )
+            |"/" -> ( / )
+            |"%" -> (mod)
+            | _  -> internal "Not an operator"; raise Terminate in
+            match (get_constant_val q1, get_constant_val q2) with 
+            |(None, None) -> quad
+            |(None, Some v) -> handle_trans (Quad_calc(op, q1, Quad_int(string_of_int v), q))
+            |(Some v, None) -> handle_trans (Quad_calc(op, Quad_int(string_of_int v), q2,  q))
+            |(Some v1, Some v2) -> let op_res = calc v1 v2 in
+            Quad_set(Quad_int(string_of_int op_res), q)
+            )
+    |_ -> quad
+
+    in List.map change_single_quad q_list
