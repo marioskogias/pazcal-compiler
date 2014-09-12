8102e4b4156673f8de389f79bbf7e5c7698ce64e
marioskogias <marioskogias@gmail.com>
2014-09-12 14:46:38 +0300

    Remove unecessary commands
    When
    mov Ax, _
    mov Ax, _
    keep only the last command

--- FinalOpt.ml
+++ FinalOpt.ml
@@ -1,1 +1,13 @@
-let optimize a = a
+open FinalSupport 
+
+let optimize asm_list = 
+    let rec help acc alist =
+     match alist with
+     |(a::b::t) -> (
+         match a,b with
+         |Mov(m1, _), Mov(m2, _) when m1 = m2 -> help (b::acc) t
+         |_ -> (help (a::acc) (b::t))
+    )
+    |_ -> acc 
+    in let rev_list = help [] asm_list
+    in List.rev rev_list
