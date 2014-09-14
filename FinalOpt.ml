7ebc64d153a05afdd5c0fb96b1a201ebe7a6cc5d
Marios Kogias <marioskogias@gmail.com>
2014-09-14 09:57:29 +0100

    Fix bug in FinalOpt

--- FinalOpt.ml
+++ FinalOpt.ml
@@ -1,13 +1,14 @@
 open FinalSupport 
 
 let optimize asm_list = 
-    let rec help acc alist =
-     match alist with
-     |(a::b::t) -> (
+  let rec help acc alist =
+    match alist with
+      |(a::b::t) -> (
          match a,b with
-         |Mov(m1, _), Mov(m2, _) when m1 = m2 -> help (b::acc) t
-         |_ -> (help (a::acc) (b::t))
-    )
-    |_ -> acc 
-    in let rev_list = help [] asm_list
-    in List.rev rev_list
+           |Mov(m1, _), Mov(m2, _) when m1 = m2 -> help (b::acc) t
+           |_ -> (help (a::acc) (b::t))
+       )
+      |[a] ->  (help (a::acc) [])
+      |[] -> acc 
+  in let rev_list = help [] asm_list
+  in List.rev rev_list
